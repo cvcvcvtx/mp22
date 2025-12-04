c20463773d767a3a32976ad8a005c9792c8a2a45
cvcvcvtx <vitalik090806@gmail.com>
2025-12-04 13:13:51 +0300

    contacts

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,8 +1,18 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ContactViewSet
 
 app_name = 'contacts'
 
+
+router = DefaultRouter()
+router.register(r'', ContactViewSet, basename='contacts')
+
 urlpatterns = [
     
+    # GET /api/v1/contacts/ - список
+    # POST /api/v1/contacts/ - создать
+    # GET /api/v1/contacts/{id}/ - карточка
+    # PUT /api/v1/contacts/{id}/ - изменить
+    path('', include(router.urls)),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Contact
+from .serializers import ContactSerializer
 
-# Create your views here.
+class ContactViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для работы с контактами.
+    Автоматически создает CRUD: список, просмотр, создание, изменение, удаление.
+    """
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    
+    filter_backends = [
+        DjangoFilterBackend, 
+        filters.SearchFilter, 
+        filters.OrderingFilter 
+    ]
+    
+    # /?company=Google&assigned_to=1
+    filterset_fields = ['company', 'assigned_to']    # фильтры (можно че то добавить, но не вижу смысла)
+
+    # /?search=Иван
+    search_fields = ['first_name', 'last_name', 'email', 'company', 'phone'] # поиск
+
+    # /?ordering=-created_at (сначала новые)
+    ordering_fields = ['created_at', 'last_name', 'first_name', 'company'] # сортировка
+    
+    ordering = ['-created_at']
+
+    def perform_create(self, serializer):
+       
+        # если у контакта не указан ответственный менеджер то им становится тот кто сделал запрос
+        if not serializer.validated_data.get('assigned_to'):
+            serializer.save(assigned_to=self.request.user)
+        else:
+            serializer.save()
+
+
